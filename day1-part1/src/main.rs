70bfe1b138157f9247ca9304a994e773beb318dc
Nicholas Nadeau <nicholas@halodi.com>
2021-12-02 14:11:35 -0500

    feat: added day1 part 1

---
+++ day1-part1/src/main.rs
@@ -0,0 +1,43 @@
+// https://adventofcode.com/2021/day/1
+
+use std::fs::File;
+use std::io::Read;
+
+fn get_vector(fname: &str) -> Vec<i32> {
+    // get string from file
+    println!("Reading file {}... ", fname);
+    let mut file = File::open(fname).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    // convert to vector of ints
+    let mut vec = Vec::new();
+    for line in contents.lines() {
+        vec.push(line.parse::<i32>().unwrap());
+    }
+
+    return vec;
+}
+
+fn count_increases(input: &[i32]) -> i32 {
+    let mut last_value = input[0];
+    let mut _count = 0;
+    for &value in input {
+        if value > last_value {
+            _count += 1;
+        }
+        last_value = value;
+    }
+
+    return _count;
+}
+
+fn main() {
+    let test_input = get_vector("src/test.txt");
+    println!("Test Input: {:?}", test_input);
+    println!("Test Result: {}", count_increases(&test_input));
+
+    let real_input = get_vector("src/real.txt");
+    println!("Test Input: {:?}", real_input);
+    println!("Test Result: {}", count_increases(&real_input));
+}
